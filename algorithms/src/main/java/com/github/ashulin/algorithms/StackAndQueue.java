0192ebcf75cd0f04c83aa5b47065a255b15e27ed
Zongwen Li <zongwen.li.tech@gmail.com>
2021-12-20 17:15:14 +0800

    逆波兰表示法：后缀表达式

--- algorithms/src/main/java/com/github/ashulin/algorithms/StackAndQueue.java
+++ algorithms/src/main/java/com/github/ashulin/algorithms/StackAndQueue.java
@@ -157,4 +157,37 @@ public class StackAndQueue {
         }
         return String.valueOf(chars, 0, top + 1);
     }
+
+    /**
+     * 根据 逆波兰表示法，求表达式的值。
+     *
+     * <p>有效的算符包括 +、-、*、/ 。每个运算对象可以是整数，也可以是另一个逆波兰表达式。
+     *
+     * <p>输入：tokens = ["2","1","+","3","*"] 输出：9 解释：该算式转化为常见的中缀算术表达式为：((2 + 1) * 3) = 9
+     */
+    @Source(150)
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (String token : tokens) {
+            if ("+".equals(token) || "-".equals(token) || "*".equals(token) || "/".equals(token)) {
+                Integer num1 = stack.pop();
+                Integer num2 = stack.pop();
+                if ("+".equals(token)) {
+                    stack.push(num2 + num1);
+                }
+                if ("-".equals(token)) {
+                    stack.push(num2 - num1);
+                }
+                if ("*".equals(token)) {
+                    stack.push(num2 * num1);
+                }
+                if ("/".equals(token)) {
+                    stack.push(num2 / num1);
+                }
+            } else {
+                stack.push(Integer.valueOf(token));
+            }
+        }
+        return stack.pop();
+    }
 }
